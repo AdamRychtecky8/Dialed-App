cd596ff98afdcf34672345f971e4d91a7ddcfcd8
AdamRychtecky <adam.rychtecky@outlook.com>
2025-07-23 17:45:18 +0000

    Test

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import WelcomeScreen from '../screens/WelcomeScreen';
-import AssessmentScreen from '../screens/AssessmentScreen'; // Placeholder for now
+import AssessmentScreen from '../screens/AssessmentScreen'; 
 
 const Stack = createNativeStackNavigator();
 
